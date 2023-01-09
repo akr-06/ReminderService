84802c299d745a56f1237eeea86406271f591b2f
Amit Kumar Roy <akumarroy86@gmail.com>
2023-01-09 19:13:12 +0530

    Setup of SMTP with nodemailer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ const app = express();
 
 const { PORT } = require('./config/serverConfig');
 
+//const { sendBasicEmail } = require('./services/email-service');
+
 app.use(express.json());
+
+
 
 const setupAndStartServer = () => {
     app.listen(PORT, ()=> {
         console.log(`Server listening at port ${PORT}`);
+
+        // sendBasicEmail(
+        //     'businessservice0206@gmail.com',
+        //     'akumarroy86@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you ?'
+        // )
     })
 }
 
